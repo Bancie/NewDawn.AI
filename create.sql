c209f3c4ac009098f22448b1941863a462c39d93
Bancie <144613141+Bancie@users.noreply.github.com>
2025-04-01 14:39:01 +0700

    up

---
+++ create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE CONGVIEC (
+    MADA int,
+    STT int,
+    TEN_CONG_VIEC nvarchar(50),
+)
+
+CREATE TABLE PHANCONG (
+    MA_NVIEN char(9),
+    MADA int,
+    STT int,
+    THOIGIAN float,
+)
+
+CREATE TABLE DEAN (
+    MADA int,
+    TENDA NVARCHAR(15),
+    DDIEM_DA NVARCHAR(15),
+    PHONG int,
+)
